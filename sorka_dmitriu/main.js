655185d02c600c72acbef93264d12ec5a0c2b561
itschoolgomother <itschoolgomother@gmail.com>
2020-01-11 14:47:51 +0200

    11.1.2020

--- sorka_dmitriu/main.js
+++ sorka_dmitriu/main.js
@@ -1,5 +1,42 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
+var ran = Math.round(Math.random() * 12)
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 canvas.style.backgroundColor = "blue";
+
+
+ctx.beginPath()
+ctx.lineWidth = 10;
+ctx.arc (canvas.width/4,273,50,0,Math.PI*2,true);
+ctx.stroke();
+ctx.closePath()
+
+ctx.beginPath()
+ctx.lineWidth = 10;
+ctx.arc (canvas.width/4*2,273,50,0,Math.PI*2,true);
+ctx.stroke();
+ctx.closePath()
+
+ctx.beginPath()
+ctx.lineWidth = 10;
+ctx.arc (canvas.width/5 *2,300,400,0,Math.PI*2,true);
+ctx.stroke();
+ctx.closePath()
+
+ctx.beginPath()
+ctx.lineWidth = 12;
+ctx.arc (canvas.width/5 *2,400,200,0,Math.PI,false);
+ctx.stroke();
+ctx.closePath()
+var cors=["red","green","black"];
+var run = Math.round(Math.random()*2);
+for (var i = 0; i < 600; i+=20) {
+	ctx.beginPath()
+	ctx.strokeStyle = cors[run];
+	ctx.moveTo(220+i,0);
+	ctx.lineTo(195+i,100);
+	ctx.lineWidth = ran;
+	ctx.stroke();
+	ctx.closePath()
+}
