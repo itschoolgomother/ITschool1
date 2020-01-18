33bf6959f05fd6b0c40786ec85b861654adef622
itschoolgomother <itschoolgomother@gmail.com>
2020-01-18 14:54:37 +0200

    18.1.2020

--- sorka_dmitriu/main.js
+++ sorka_dmitriu/main.js
@@ -4,26 +4,41 @@ var ran = Math.round(Math.random() * 12)
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 canvas.style.backgroundColor = "blue";
-
-
+function triangle(x1,y1,x2,y2,x3,y3) {
 ctx.beginPath()
-ctx.lineWidth = 10;
-ctx.arc (canvas.width/4,273,50,0,Math.PI*2,true);
+ctx.moveTo(x1,y1)
+ctx.lineTo(x2,y2)
+ctx.lineTo(x3,y3)
+ctx.lineTo(x1,y1)
+ctx.fill();
+ctx.closePath()
+}
+var a87 = innerWidth/2;
+var s87 = innerHeight/2;
+function equalside(x,y,size) {
+ctx.lineWidth=10	
+ctx.beginPath()
+ctx.moveTo(x-size/2,y+size/2)
+ctx.lineTo((x-size/2)+size,y+size/2)
+ctx.lineTo(x,(y+size/2)-size)
+ctx.lineTo(x-size/2,y+size/2)
+ctx.fill();
+ctx.closePath()
+}
+equalside(a,s,500);
+//triangle(300,300,400,100,500,300);
+/*ctx.beginPath()
+ctx.moveTo(300,300)
+ctx.lineTo(600,300)
 ctx.stroke();
 ctx.closePath()
 
 ctx.beginPath()
-ctx.lineWidth = 10;
-ctx.arc (canvas.width/4*2,273,50,0,Math.PI*2,true);
+ctx.moveTo(300,300)
+ctx.lineTo(500,300)
 ctx.stroke();
 ctx.closePath()
-
-ctx.beginPath()
-ctx.lineWidth = 10;
-ctx.arc (canvas.width/5 *2,300,400,0,Math.PI*2,true);
-ctx.stroke();
-ctx.closePath()
-
+/*
 ctx.beginPath()
 ctx.lineWidth = 12;
 ctx.arc (canvas.width/5 *2,400,200,0,Math.PI,false);
@@ -40,3 +55,4 @@ for (var i = 0; i < 600; i+=20) {
 	ctx.stroke();
 	ctx.closePath()
 }
+*/
