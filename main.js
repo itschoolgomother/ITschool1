8f879202143955ed2b905d5fca138c6eb05f0d75
itschoolgomother <itschoolgomother@gmail.com>
2020-01-19 19:32:43 +0200

    19.1.2020

---
+++ main.js
@@ -0,0 +1,5 @@
+alert("Oks" + name)
+var box = document.getElementsById("box");
+box.style.width="100px"
+box.style.heigth="100px"
+box.style.backgroundColor ="red"
---
+++ saha/main.js
@@ -0,0 +1,5 @@
+alert("Oksdxf" + name)
+var box = document.getElementById("box");
+box.style.width="100px"
+box.style.height="100px"
+box.style.backgroundColor ="red"
